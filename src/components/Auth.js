c9b9bd9ea0f353545cd65b381249f2bc13847ef9
74NV1R <mhasan182021@bscse.uiu.ac.bd>
2024-01-31 22:52:23 +0600

    auth form added

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,9 +1,68 @@
-import React from 'react'
+import React, { Component } from "react"
+import { Formik } from 'formik'
 
-const Auth = () => {
-    return (
-        <div>Auth</div>
-    )
+
+class Auth extends Component {
+
+    render() {
+        return (
+            <div className="container">
+                <Formik
+                    initialValues={{
+                        email: "",
+                        password: "",
+                        passwordConfirm: ""
+                    }}
+
+                    onSubmit={(values) => {
+                        console.log(values)
+                    }
+                    }
+                >
+
+                    {({ values, handleChange, handleSubmit }) => (<div>
+                        <form onSubmit={handleSubmit}>
+                            <br />
+
+                            <input
+                                name="email"
+                                placeholder="Enter your email"
+                                className="form-control"
+                                value={values.email}
+                                onChange={handleChange}
+                            />
+                            <br />
+                            <input
+                                name="password"
+                                placeholder="Password"
+                                className="form-control"
+                                value={values.password}
+                                onChange={handleChange}
+                            />
+                            <br />
+                            <input
+                                name="passwordConfirm"
+                                placeholder="Confirm Password"
+                                className="form-control"
+                                value={values.passwordConfirm}
+                                onChange={handleChange}
+                            />
+                            <br />
+
+                            <button type="submit" className="btn btn-success">
+                                Sign up
+                            </button>
+
+
+                        </form>
+                    </div>)}
+
+
+                </Formik>
+            </div>
+        )
+
+    }
 }
 
 export default Auth
