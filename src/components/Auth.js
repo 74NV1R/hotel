d71d3eab8693cf42aff5e34ea3fc6d5777edac6d
74NV1R <mhasan182021@bscse.uiu.ac.bd>
2024-01-31 23:02:53 +0600

    Auth updated

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -4,6 +4,16 @@ import { Formik } from 'formik'
 
 class Auth extends Component {
 
+    state = {
+        mode: 'Sign up'
+    }
+
+    switchMode = () => {
+        this.setState({
+            mode: this.state.mode === "Sign Up" ? 'Login' : 'Sign Up'
+        })
+    }
+
     render() {
         return (
             <div className="container">
@@ -23,6 +33,13 @@ class Auth extends Component {
                     {({ values, handleChange, handleSubmit }) => (<div>
                         <form onSubmit={handleSubmit}>
                             <br />
+                            <button style={{
+                                width: '100%',
+                                backgroundColor: '#570303',
+                                color: 'white'
+                            }} className="btn btn-class" onClick={this.switchMode}>Switch to {this.state.mode === 'Sign Up' ? "Login" : "Sign up"}</button>
+                            <br />
+                            <br />
 
                             <input
                                 name="email"
@@ -40,17 +57,17 @@ class Auth extends Component {
                                 onChange={handleChange}
                             />
                             <br />
-                            <input
-                                name="passwordConfirm"
-                                placeholder="Confirm Password"
-                                className="form-control"
+                            {this.state.mode === "Sign Up" ? <div><input name="passwordConfirm" placeholder="Confirm Password" className="form-control"
                                 value={values.passwordConfirm}
-                                onChange={handleChange}
-                            />
-                            <br />
+                                onChange={handleChange} />
+                                <span style={{ color: "red" }}>
+                                </span>
+                                <br /></div> : null}
+
+
 
                             <button type="submit" className="btn btn-success">
-                                Sign up
+                                {this.state.mode === 'Sign Up' ? 'Sign Up' : 'Login'}
                             </button>
 
 
