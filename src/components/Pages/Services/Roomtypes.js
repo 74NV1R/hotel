dea63f1141f8b0bdc6797759414e534c590f4ec4
74NV1R <mhasan182021@bscse.uiu.ac.bd>
2024-02-08 23:48:54 +0600

    post method

--- src/components/Pages/Services/Roomtypes.js
+++ src/components/Pages/Services/Roomtypes.js
@@ -1,8 +1,26 @@
 import { Modal, Button, ModalBody, ModalFooter, ModalHeader } from 'reactstrap'
 import React, { useState } from 'react'
+import { useAppContext } from '../../../Context'
+import axios from 'axios'
 
 const Roomtypes = ({ type, charge }) => {
 
+    const { items, updateItems } = useAppContext()
+
+
+    const handlePurchase = (amount) => {
+        updateItems({
+            cost: charge,
+            services: [...items.services, roomType],
+            checkInTime: new Date() + selectedNumber,
+            holdingTime: 0
+        })
+
+        console.log(items)
+    }
+
+
+    const [roomType, setRoomType] = useState(null)
     const [expand, setExpand] = useState(false)
     const [book, setBook] = useState(false)
     const [selectedNumber, setSelectedNumber] = useState(1)
@@ -13,6 +31,7 @@ const Roomtypes = ({ type, charge }) => {
 
 
     const toggleExpand = () => {
+        setRoomType(type)
         setExpand(!expand)
     }
 
@@ -21,6 +40,33 @@ const Roomtypes = ({ type, charge }) => {
     }
 
     const bookRoom = () => {
+        setModal(false)
+        if (!book) {
+            const bookingDate = new Date()
+            setDate(bookingDate)
+        }
+        else {
+            const currentDate = new Date()
+            const futureDate = new Date(currentDate.getTime() + selectedNumber * 24 * 60 * 60 * 1000)
+            setDate(futureDate)
+        }
+        handlePurchase()
+        console.log(items)
+        postUserData()
+
+    }
+
+    const postUserData = () => {
+        axios.post("https://hotel-6952b-default-rtdb.firebaseio.com/users.json", items)
+            .then(response => {
+                if (response.status === 200) {
+                    console.log('posted')
+                    //onClose()
+                }
+            })
+            .catch(err => {
+                console.log(err.response)
+            })
 
     }
 
@@ -39,17 +85,17 @@ const Roomtypes = ({ type, charge }) => {
     return (
         <div className='col-3'>
             <br />
-            <div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 hover:drop-shadow-2xl dark:border-gray-700 dark:hover:bg-gray-700">
+            <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 hover:drop-shadow-2xl dark:border-gray-700 dark:hover:bg-gray-700">
                 <div onClick={toggleExpand}>
                     <h3>{type}</h3>
-                    <span class="bg-blue-100 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 cursor-pointer rounded-full dark:bg-blue-900 dark:text-blue-300">Click to {!expand ? 'book' : 'hide'}</span>
+                    <span className="bg-blue-100 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 cursor-pointer rounded-full dark:bg-blue-900 dark:text-blue-300">Click to {!expand ? 'book' : 'hide'}</span>
 
                 </div>
                 {expand && (
                     <div className="card-body">
                         <p>{charge}$ daily</p>
-                        <button type="button" class="text-white bg-red-900 hover:bg-red-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-900 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800" onClick={toggle}>Check in today</button>
-                        <button type="button" class="text-white bg-red-900 hover:bg-red-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-900 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800" onClick={toggleBook} >Book for later</button>
+                        <button type="button" className="text-white bg-red-900 hover:bg-red-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-900 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800" onClick={toggle}>Check in today</button>
+                        <button type="button" className="text-white bg-red-900 hover:bg-red-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-900 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800" onClick={toggleBook} >Book for later</button>
 
                     </div>
                 )}
@@ -68,7 +114,7 @@ const Roomtypes = ({ type, charge }) => {
                     </select>
                     <p>Your check-in date: {calculateFutureDate(selectedNumber)}</p>
                     <br />
-                    <button type="button" class="text-white bg-red-900 hover:bg-red-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-900 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800" onClick={setModal} >Book</button>
+                    <button type="button" className="text-white bg-red-900 hover:bg-red-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-900 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800" onClick={setModal} >Book</button>
 
                 </span> : null}
 
